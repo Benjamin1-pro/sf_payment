c4edd827012489082eb00ff9d9413f08f410e06a
benjamin1-pro <muthamubenjamin@gmail.com>
2020-12-24 15:32:22 -0500

    transaction_api modifications

--- transaction_api/objects/transactions.php
+++ transaction_api/objects/transactions.php
@@ -1,37 +1,43 @@
 <?php
-
-class Transactions
- {
-   // database connection and table name
+class Transactions{
+  
+    // database connection and table name
     private $conn;
     private $table_name = "transactions";
-
+  
     // object properties
     public $id;
     public $amount;
     public $bankslip_number;
     public $account_number;
     public $account_name;
-    public $category_name;
     public $reason;
     public $payment_date;
+  
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
 
-  // constructor with $db as database connection
-   function __construct($db)
-   {
-     $this->conn = $db;
-   }
+    // read transactions
+    function read(){
+  
+    // select all query
+    $query = "SELECT
+                    id, amount, bankslip_number, account_number, account_name, reason, payment_date
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    payment_date";
+  
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+  
+    // execute query
+    $stmt->execute();
+  
+    return $stmt;
+    }
+}
 
-   function read_transaction()
-   {
-     $query = "SELECT id, amount, bankslip_number, account_number, category_name, reason, payment_date FROM transactions";
-
-     // prepare query statement
-     $stmt = $this->conn->prepare($query);
-
-     // execute query
-     $stmt->execute();
-
-     return $stmt;
-   }
- }
+?>
--- transaction_api/transactions/transactionList.php
+++ transaction_api/transactions/transactionList.php
@@ -1,61 +1,61 @@
 <?php
-header("Access-Control-Origin: *");
+// required headers
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
+  
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/transactions.php';
-
-// instantiate database and transactions object
+  
+// instantiate database and transaction object
 $database = new Database();
 $db = $database->getConnection();
-
+  
 // initialize object
-$transaction_list = new Transactions($db);
-
-// query Transactions
-$stmt = $transaction_list->read_transaction();
+$transaction = new Transactions($db);
+  
+// query 
+$stmt = $transaction->read();
 $num = $stmt->rowCount();
-
+  
 // check if more than 0 record found
-if ($num>0) {
-
-  // products array
-  $transaction_arr = array();
-  $transaction_arr['records'] = array();
-
-  // retrieve our table contents
-  while ($row- = $stmt->fecth(PDO::FECTH_ASSOC)) {
-
-    extract($row);
-
-    $transactionItem = array(
-      "id"                => $id,
-      "amount"            => $amount,
-      "bankslip_number"   => $bankslip_number,
-      "account_number"    => $account_number,
-      "account_name"      => $account_name,
-      "reason"            => $reason,
-      "payment_date"      => $payment_date
-    );
-
-    array_push($transaction_arr['records'], $transactionItem);
-
+if($num>0){
+  
+    // transaction array
+    $transaction_arr=array();
+    $transaction_arr["records"]=array();
+  
+    // retrieve our table contents
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        extract($row);
+  
+        $transaction_data=array(
+            "id" => $id,
+            "amount" => $amount,
+            "bankslip_number" => $bankslip_number,
+            "account_number" => $account_number,
+            "account_name" => $account_name,
+            "reason" => $reason,
+            "payment_date" => $payment_date,
+        );
+  
+        array_push($transaction_arr["records"], $transaction_data);
+    }
+  
     // set response code - 200 OK
     http_response_code(200);
-
+  
     // show transactions data in json format
     echo json_encode($transaction_arr);
-
-  }
-  else {
-
+}
+  else{
+  
     // set response code - 404 Not found
     http_response_code(404);
-
+  
     // tell the user no transaction found
     echo json_encode(
         array("message" => "No transaction found.")
     );
-  }
 }
